95419f57133e87c6c6b836b04fb1e2b8ce5dde02
Santiago Neira <sant.neira@profesor.duoc.cl>
2016-11-08 01:03:48 -0300

    Initial commit

---
+++ app/src/main/java/cl/duoc/android/camara/MainActivity.java
@@ -0,0 +1,56 @@
+package cl.duoc.android.camara;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Constante utilizada en el Intent
+    private static final int REQUEST_CODE = 1;
+
+    // Bitmap donde almacenamos la imagen
+    private Bitmap bitmap;
+
+    // Vista del tipo Imagen de la interfaz de usuario
+    private ImageView imageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Recuperamos el ImageView
+        imageView = (ImageView) findViewById(R.id.foto);
+    }
+
+    // En el layout hemos definido un botón asociado a este método al hacer click
+    public void capturaImagen(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_CODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            bitmap = (Bitmap) bundle.get("data");
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+
+
+}
